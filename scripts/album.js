db97ea5f352048d373184b3e02a13ca444cb0213
Steve Edwards <anthromt@gmail.com>
2017-01-28 11:48:11 -0700

    checkpoint 12 complete

--- scripts/album.js
+++ scripts/album.js
@@ -54,7 +54,7 @@ var albumWilco = {
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -85,6 +85,11 @@ var albumArtist;
 var albumReleaseInfo;
 var albumImage;
 var albumSongList;
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  
  window.onload = function() {
      albumTitle = document.getElementsByClassName('album-view-title')[0];
@@ -93,6 +98,22 @@ var albumSongList;
      albumImage = document.getElementsByClassName('album-cover-art')[0];
      albumSongList = document.getElementsByClassName('album-view-song-list')[0];
      setCurrentAlbum(albumPicasso);
+     
+      songListContainer.addEventListener('mouseover', function(event) {
+         // #1
+         console.log(event.target);
+         if (event.target.parentElement.className === 'album-view-song-item') {
+         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+     });
+ ...
+         } 
+     });
+ for (var i = 0; i < songRows.length; i++) {
+         songRows[i].addEventListener('mouseleave', function(event) {
+         this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
      
      var albumArray = [albumPicasso, albumMarconi, albumWilco];
      var list = 1;
