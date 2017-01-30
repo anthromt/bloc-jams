071b41a86f32876ef528553eeadcc693aceb86f3
Steve Edwards <anthromt@gmail.com>
2017-01-30 10:33:13 -0700

    style sheets updated

--- scripts/album.js
+++ scripts/album.js
@@ -97,32 +97,27 @@ var albumImage;
 var albumSongList;
 
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-<<<<<<< HEAD
 
-=======
 var songRows = document.getElementsByClassName('album-view-song-item');
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  
->>>>>>> checkpoint-12
- window.onload = function() {
-     albumTitle = document.getElementsByClassName('album-view-title')[0];
-     albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     albumImage = document.getElementsByClassName('album-cover-art')[0];
-     albumSongList = document.getElementsByClassName('album-view-song-list')[0];
-     setCurrentAlbum(albumPicasso);
-     
-      songListContainer.addEventListener('mouseover', function(event) {
-         // #1
-         console.log(event.target);
-         if (event.target.parentElement.className === 'album-view-song-item') {
-         event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-         }
-     });
- ...
-         } 
-     });
+window.onload = function() {
+ albumTitle = document.getElementsByClassName('album-view-title')[0];
+ albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ albumImage = document.getElementsByClassName('album-cover-art')[0];
+ albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ setCurrentAlbum(albumPicasso);
+
+  songListContainer.addEventListener('mouseover', function(event) {
+     // #1
+     console.log(event.target);
+     if (event.target.parentElement.className === 'album-view-song-item') {
+     event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+     }
+  });
+
  for (var i = 0; i < songRows.length; i++) {
          songRows[i].addEventListener('mouseleave', function(event) {
          this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
